653ea98ed6c79939abe68760163dd36d186cd03b
Chandler Rose <109770223+Turbul3nce@users.noreply.github.com>
2024-12-04 15:35:14 -0500

    Added lines for coc.vim install and config

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Setup VM
 apt install open-vm-tools-desktop
 apt install open-vm-tools
@@ -74,4 +75,33 @@ go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 cat .bashrc.new >> ~/.bashrc 
 source ~/.bashrc
 
+#coc.vim 
+
+set -o nounset    # error when referencing undefined variable
+set -o errexit    # exit when command fails
+
+# Install latest nodejs
+if [ ! -x "$(command -v node)" ]; then
+    curl --fail -LSs https://install-node.now.sh/latest | sh
+    export PATH="/usr/local/bin/:$PATH"
+    # Or use package manager, e.g.
+    # sudo apt-get install nodejs
+fi
+
+# Use package feature to install coc.nvim
+
+# for vim8
+mkdir -p ~/.vim/pack/coc/start
+cd ~/.vim/pack/coc/start
+curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
+
+# Install extensions
+mkdir -p ~/.config/coc/extensions
+cd ~/.config/coc/extensions
+if [ ! -f package.json ]
+then
+  echo '{"dependencies":{}}'> package.json
+fi
+# Change extension names to the extensions you need
+npm install coc-snippets --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 
